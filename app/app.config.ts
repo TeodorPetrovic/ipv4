7a7760c3737b381146926cb29eef68b66710654e
TeodorPetrovic <63924993+TeodorPetrovic@users.noreply.github.com>
2026-03-12 13:14:33 +0100

    fix: Added better theme

---
+++ app/app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      primary: 'red',
+      secondary: 'orange',
+      success: 'emerald',
+      info: 'sky',
+      warning: 'amber',
+      error: 'rose',
+      neutral: 'zinc',
+    },
+  },
+})
